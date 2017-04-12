be5c4c23caed471df5f7074d1e1777621bbec8df
Alex Vohs <Alex Vohs>
2017-04-12 17:54:00 -0400

    idk

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'Ctj8Q6J6PHntzVlSRfRzh6ujo',
+  consumer_secret: 'wJXXLOAWpR8ezvDTswTWDQ9u9OYuN5QYuEfsQkKrTIKJCo411N',
+  access_token_key: '	852167073438552064-zk2kYJfI13ld387xocsnfjHcLpbG9z4',
+  access_token_secret: 'p8M3sksDNuEreeF9XcnYTdntkXczeoW8NHGsC3i9JBYuI',
+}
